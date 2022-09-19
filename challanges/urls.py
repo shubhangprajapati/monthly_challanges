7a9bbbe8ee6b4d11ab7b06754ef240c45701a5cf
shubhanggg12 <shubhangprajapati19@gnu.ac.in>
2022-09-19 16:36:15 +0530

    third commit

--- challanges/urls.py
+++ challanges/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 
 urlpatterns = [
+    path("", views.index ),#challanges
     path("<int:month>",views.monthly_challanges_by_number),
-    path("<str:month>",views.monthly_challanges)
+    path("<str:month>",views.monthly_challanges, name="month-challange")
 ]
 
 
--- challanges/views.py
+++ challanges/views.py
@@ -1,5 +1,8 @@
+import imp
+from urllib import response
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 monthly_challangess = {
@@ -17,6 +20,16 @@ monthly_challangess = {
     "december": "study django for 20 mins atleast!"
 }
 
+def index(request):
+    list_items =""
+    months = list(monthly_challangess.keys())
+    
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challange", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    response_data=f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
 
 def monthly_challanges_by_number(request, month):
 
@@ -25,12 +38,14 @@ def monthly_challanges_by_number(request, month):
         return HttpResponseNotFound("invalid month")
 
     redirect_month = months[month-1]
-    return HttpResponseRedirect(redirect_month)
+    redirect_path = reverse("month-challange",args=[redirect_month]) # challenge/january 
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challanges(request, month):
     try:
         challange_text = monthly_challangess[month]
-        return HttpResponse(challange_text)
+        response_data=f"<h1>{challange_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("this month does not match!")
+        return HttpResponseNotFound("<h1>this month does not match!</h1>")
